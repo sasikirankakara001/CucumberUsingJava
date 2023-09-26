8d9879e3d71352f3799a9611ae416ab3f87a08e7
SasikiranKakara <55825519+sasikirankakara001@users.noreply.github.com>
2023-09-27 04:14:59 +0530

    commit

--- CucumberLearningJava/src/test/java/features/firstProgram.feature
+++ CucumberLearningJava/src/test/java/features/firstProgram.feature
@@ -6,3 +6,11 @@ When User login into application with "admin" and "password"
 Then Home Page is displayed
 And Cards are displayed
 
+# Reusable
+
+Scenario: User Page default login 
+
+Given User is on NetBanking landing page
+When User login into application with "<username>" and "<password>"
+Then Home Page is displayed
+And Cards are displayed
--- CucumberLearningJava/src/test/java/stepDefinitions/MainSteps.java
+++ CucumberLearningJava/src/test/java/stepDefinitions/MainSteps.java
@@ -13,10 +13,10 @@ public class MainSteps {
 		System.out.println("Welcome to Land Page");
 	}
 	@When("User login into application with {string} and {string}")
-	public void user_login_into_application_with_and(String string, String string2) {
+	public void user_login_into_application_with_and(String username, String password) {
 	    // Write code here that turns the phrase above into concrete actions
 //	    throw new io.cucumber.java.PendingException();
-		System.out.println("Welcome to the User Login");
+		System.out.println("UserName: "+username+" "+"Password: "+password);
 	}
 	@Then("Home Page is displayed")
 	public void home_page_is_displayed() {
