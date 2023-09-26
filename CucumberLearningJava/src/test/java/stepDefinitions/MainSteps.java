86113f7883988fdd60e3c787a6a7919f606a4a49
SasikiranKakara <55825519+sasikirankakara001@users.noreply.github.com>
2023-09-27 04:03:12 +0530

    commit

--- CucumberLearningJava/src/test/java/stepDefinitions/MainSteps.java
+++ CucumberLearningJava/src/test/java/stepDefinitions/MainSteps.java
@@ -1,8 +1,35 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class MainSteps {
 	
-	
+	@Given("User is on NetBanking landing page")
+	public void user_is_on_net_banking_landing_page() {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+		System.out.println("Welcome to Land Page");
+	}
+	@When("User login into application with {string} and {string}")
+	public void user_login_into_application_with_and(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+		System.out.println("Welcome to the User Login");
+	}
+	@Then("Home Page is displayed")
+	public void home_page_is_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+		System.out.println("Welcome to the Home Page");
+	}
+	@Then("Cards are displayed")
+	public void cards_are_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+		System.out.println("Cards are displayed!!");
+	}	
 	
 
 }
